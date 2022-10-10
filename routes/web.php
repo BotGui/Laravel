81093173d98f8d8d8aac4d237389349cbbae1b37
Guilherme <guilherme@sentry.com>
2022-10-10 08:53:29 -0300

    teste

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
+ 
 Route::get('/series', [SeriesController::class, 'index']);
 
 Route::get('/series/criar', [SeriesController::class, 'create']);
